a5b69ddfd50c3f1336e4e2be98fb221330a754e7
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-03-30 00:28:00 -0300

    FIX: Ajustando os testes dde teams e users

--- app/backend/src/tests/teams.model.test.ts
+++ app/backend/src/tests/teams.model.test.ts
@@ -14,12 +14,25 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Testa a model de teams', () => {
-  it('Retorna todos os tiimes', async () => {
+  afterEach(() => {
+    sinon.restore();
+  })
+
+  it('Retorna todos os times', async () => {
     sinon.stub(modelTeams, 'findAll').resolves(allTeams);
 
     const result: Response = await chai.request(app).get('/teams');
 
     expect(result.status).to.be.equal(200);
     expect(result.body).to.be.deep.equal(allTeams);
+  })
+
+  it('Retorna um time pelo id', async () => {
+    sinon.stub(modelTeams, 'findByPk').resolves(allTeams[0]);
+
+    const result: Response = await chai.request(app).get('/teams/1');
+
+    expect(result.status).to.be.equal(200);
+    expect(result.body).to.be.deep.equal(allTeams[0]);
   })
 })
--- app/backend/src/tests/users.model.test.ts
+++ app/backend/src/tests/users.model.test.ts
@@ -5,7 +5,7 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app'
 import modelUser from '../database/models/User';
-import { allUsers } from './mocks/users.mock';
+import { UserOne } from './mocks/users.mock';
 
 import { Response } from 'superagent';
 
@@ -13,13 +13,58 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testa a model de teams', () => {
-  it('Retorna todos os tiimes', async () => {
-    sinon.stub(modelUser, 'findAll').resolves(allUsers);
+describe('Testa a model de Users', () => {
+  afterEach(() => {
+    sinon.restore();
+  })
 
-    const result: Response = await chai.request(app).get('/login');
+  it('Retorna o token de um login', async () => {
+    sinon.stub(modelUser, 'findOne').resolves(UserOne[0]);
+
+    const result: Response = (await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com', password: 'admin' }));
+    
 
     expect(result.status).to.be.equal(200);
-    expect(result.body).to.be.deep.equal(allUsers);
+    expect(result.body.token).to.be.includes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9");
+  })
+
+  it('Retorna um erro ao faltar credenciais', async () => {
+    const result: Response = (await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com' }));
+    
+
+    expect(result.status).to.be.equal(400);
+    expect(result.body).to.be.deep.equal({ message: 'All fields must be filled' });
+  })
+
+  it('Retorna um erro quando a senha não é válida', async () => {
+    sinon.stub(modelUser, 'findOne').resolves(UserOne[0]);
+
+    const result: Response = (await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com', password: 'administrador' }));
+    
+
+    expect(result.status).to.be.equal(401);
+    expect(result.body).to.be.deep.equal({ message: 'Email or password invalid' });
+  })
+
+  it('Retorna um erro quando o email não é válida', async () => {
+    sinon.stub(modelUser, 'findOne').resolves(null);
+
+    const result: Response = (await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'administrador@admin.com', password: 'admin' }));
+    
+
+    expect(result.status).to.be.equal(401);
+    expect(result.body).to.be.deep.equal({ message: 'Email or password invalid' });
   })
 })
